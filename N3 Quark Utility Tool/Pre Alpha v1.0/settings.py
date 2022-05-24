959fbc6f24775de743cc3c2984fa3681f81033f9
p-i-c-o <95228665+p-i-c-o@users.noreply.github.com>
2022-05-24 10:04:08 +0200

    Create settings.py

---
+++ N3 Quark Utility Tool/Pre Alpha v1.0/settings.py
@@ -0,0 +1,9 @@
+# SETTINGS
+
+# !!! Replace the examples with your own !!! 
+
+
+PicoPath = "/Volumes/N3QUARK"
+UsbPath = "/Volumes"
+
+ScriptsPath = "/Users/elie/Desktop/N3 Quark GUI Backups/v1.0/Scripts"
